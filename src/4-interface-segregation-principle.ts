52afab1db8bece5a63ea1ca798e5170384d43fe5
Cristiano Ventura <mrk.cristiano@gmail.com>
2019-06-08 23:09:04 -0400

    Add ISP principle

---
+++ src/4-interface-segregation-principle.ts
@@ -0,0 +1,62 @@
+/**
+ * Interface Segregation Principle
+ * @description Don't expose methods to your clients, methods that they don't use
+ * @example An interface that should implement methods that cannot be used
+ **/
+
+interface Animal {
+	fly(): void;
+	walk(): void;
+	bark(): void;
+	meow(): void;
+}
+
+// Implementing this interface will be weird, cause some methods are not consistents for some cases
+class Dog implements Animal {
+	fly(): void {
+		// Dogs don't fly - this method should not be in the the Animal interface
+	}
+
+	walk(): void {
+		// ok
+   	}
+
+    bark(): void {
+	// ok
+	}
+
+	meow(): void {
+		// ????
+   	}
+ }
+
+
+
+// ============> Interface Segregation Principle
+// Separate methods in different interfaces that they belong
+interface IDog {
+	bark(): void;
+}
+
+interface ICat {
+	meow(): void;
+}
+
+interface IBird {
+	fly(): void;
+}
+
+interface IQuadrupeds {
+	walk(): void;
+}
+
+// Now we are implementing only the interfaces and methods we need
+class Dog implements IDog, IQuadrupeds {
+	bard(): void {
+		// ...
+	}
+
+	walk(): void {
+		// ...
+	}
+}
